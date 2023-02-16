5ba689dac80028f01597a70b01e259aee10ad58b
Geraldo-dev <laboratorio.inicial@gmail.com>
2023-02-16 07:47:37 -0300

    Criacao do routes

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,15 +1,15 @@
+import 'dotenv/config'
 import express from 'express'
 import 'express-async-errors'
 import cors from 'cors'
+import { routes } from './routes'
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Olá dev!' })
-})
+app.use(routes)
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000')
+app.listen(process.env.PORT, () => {
+  console.log(`Server started on port ${process.env.PORT}`)
 })
