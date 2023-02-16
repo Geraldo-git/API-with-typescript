90ebe2da79842d8a7c95d8b4db35872a34347bd3
Geraldo-dev <laboratorio.inicial@gmail.com>
2023-02-16 07:21:52 -0300

    Adicionados lib, cors e express

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,7 +1,15 @@
-class AppServer {
-  private app: string;
+import express from 'express'
+import 'express-async-errors'
+import cors from 'cors'
 
-  constructor(info: string) {
-    this.app = info ?? "Olá dev!";
-  }
-}
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'Olá dev!' })
+})
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000')
+})
